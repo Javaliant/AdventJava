facabb30d6cd9403e2d9d9e00b596b732cf86ee0
Luigi Vincent <luigivincenthr@gmail.com>
2014-09-07 13:04:10 -0400

    Update FizzBuzzCounter.java

--- FizzBuzzCounter.java
+++ FizzBuzzCounter.java
@@ -24,10 +24,10 @@ public class FizzBuzzCounter {
     public static void printFizzBuzz(int n) {
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0) sb.append("Fizz");
-            if (i % 5 == 0) sb.append("Buzz");
-            if (sb.length() == 0) System.out.println(i);
-            else System.out.println(sb.toString());
+            if (i % 3 == 0) { sb.append("Fizz"); }
+            if (i % 5 == 0) { sb.append("Buzz"); }
+            if (sb.length() == 0) { System.out.println(i); }
+            else { System.out.println(sb.toString()); }
             sb.setLength(0);
         }
     }
