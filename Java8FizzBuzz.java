5bb6fb78acd638da75cb1ca8512e2f59d41aaf8a
Luigi Vincent <luigivincenthr@gmail.com>
2015-02-06 17:12:59 -0500

    Create Java8FizzBuzz.java

---
+++ Java8FizzBuzz.java
@@ -0,0 +1,27 @@
+/* Author: Luigi Vincent
+Quick exercise to remmeber java 8
+*/
+
+import java.util.stream.IntStream;
+
+public class Java8FizzBuzz {
+	final static int FIZZ = 3;
+	final static int BUZZ = 5;
+
+	public static void main(String[] args) {
+		IntStream.rangeClosed(1, 100).forEach(Java8FizzBuzz::fizzBuzzify);
+	}
+
+	private static void fizzBuzzify(int i) {
+		StringBuilder sb = new StringBuilder();
+
+		if (i % FIZZ == 0) {
+			sb.append("Fizz");
+		}
+		if (i % BUZZ == 0) {
+			sb.append("Buzz");
+		}
+
+		System.out.println(sb.length() > 0 ? sb.toString() : i);
+	}
+}
