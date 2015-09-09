870d5d21716f8343418bd0ea105e5561cf376be0
Luigi Vincent <luigipvincent@gmail.com>
2015-09-08 21:06:20 -0400

    Update SimpleServer.java

--- SimpleServer.java
+++ SimpleServer.java
@@ -1,31 +1,46 @@
-/* Author: Luigi Vincent */
+/* Author: Luigi Vicnent */
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.Date;
 
+public class SimpleServer {
+	final static int PORT = 5290;
 
-public class SimpleClient {
-	static final int PORT = 5290;
-	static final String SERVER_ADDRESS = "localhost";
-	static Scanner keyboard = new Scanner(System.in);
-	static BufferedReader in;
-	static PrintWriter out;
+	public static void main(String[] args) {
 
-	public static void main(String[] args) throws IOException {
-		Socket socket = new Socket(SERVER_ADDRESS, PORT);
-		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		out = new PrintWriter(socket.getOutputStream(), true);
+		try(ServerSocket server = new ServerSocket(PORT)) {
+			System.out.println(new Date() + "\nServer online.");
 
-		while (true) {
-			String input = in.readLine();
-			if (input.equals("Hello")) {
-				System.out.println("What is your name?");
-				out.println(keyboard.nextLine());
+			while(true) {
+				new ClientHandler(server.accept()).start();
 			}
+		} catch (IOException ioe) {}	
+	}
+
+	private static class ClientHandler extends Thread {
+		private Socket socket;
+		private BufferedReader in;
+		private PrintWriter out;
+
+		ClientHandler(Socket socket) {
+			this.socket = socket;
+			System.out.println("Client connected!");
+		}
+
+		@Override
+		public void run() {
+			try {
+				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				out = new PrintWriter(socket.getOutputStream(), true);
+				out.println("Hello");
+				String userName = in.readLine();
+				System.out.println("Name is " + userName);
+			} catch (IOException ioe) {}
 		}
 	}
 }
